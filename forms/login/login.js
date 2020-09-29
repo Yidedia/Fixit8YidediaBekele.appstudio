bbf7b514190214776ee203423b59512ee906c4f9
Yidedia <70745425+Yidedia@users.noreply.github.com>
2020-09-28 19:56:18 -0500

    member
    label works

--- forms/login/login.js
+++ forms/login/login.js
@@ -1,4 +1,4 @@
-/*
+
 let accountName = ""
 
 btnSubmit.onclick=function(){
@@ -11,4 +11,7 @@ btnSubmit.onclick=function(){
     inptAccount.value = ""
     inptPassword.value = ""
 }
-*/
+ 
+membtn.onclick=function(){
+  ChangeForm(membership)
+}
--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,14 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary", "Sam", "Joey", "Paul", "Mike", "Char", "Jenny", "Kennedy", "Kaden", "Kurt", "Anne"]
+
+let userName = ''
+
+memSubmit.onclick = function() {
+  userName = memberInput.value
+  if (members.includes(userName) == true) {
+    memLabel.value = "They are a member"
+  } else {
+    memLabel.value = "They are not a member, but your name has been added now"
+    members.push(userName)
+  }
+}
